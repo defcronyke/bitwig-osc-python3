de36d5bfea2970eba68c5c123db2d83391148674
Jeremy Carter <Jeremy@JeremyCarter.ca>
2018-10-09 04:17:15 -0400

    implement play_note() and some other related stuff
    API route: /vkb_midi/{Channel:0-16}/note/{Note:0-127} {Velocity:0-127}

---
+++ bitwig_osc.py
@@ -0,0 +1,80 @@
+""" Bitwig OSC client example - Daphne1 """
+import signal
+import sys
+from pythonosc import osc_message_builder
+from pythonosc import udp_client
+
+
+class BitwigOSC:
+    """ A client library for the Moss Bitwig OSC API, to control Bitwig with 
+        the Open Sound Control protocol.
+        ( API: https://github.com/git-moss/DrivenByMoss/wiki/Open-Sound-Control-(OSC) ) """
+
+    def __init__(self, ip="127.0.0.1", port=8000, chan=1):
+        """ Set the OSC server's IP and port while instantiating the OSC client, 
+            and set the default MIDI channel to use. """
+        signal.signal(signal.SIGINT, self.signal_handler)
+
+        # Save some vars for later.
+        self.ip = ip        # The OSC server IP.
+        self.port = port    # The OSC server port.
+        self.chan = chan    # The default MIDI channel to send on.
+        self.notes_on = {}  # The notes that are currently on.
+
+        # Instantiate an OSC UDP client.
+        self.client = udp_client.SimpleUDPClient(ip, port)
+
+    def play_note(self, note, vel, chan=None):
+        """ Play Bitwig virtual MIDI keyboard using OSC
+            API route: /vkb_midi/{Channel:0-16}/note/{Note:0-127} {Velocity:0-127} """
+        # Use default MIDI channel if chan argument not specified.
+        if chan == None:
+            chan = self.chan
+
+        # If we are sending a note velocity larger than 0, set the note as on
+        # in our records.
+        if vel > 0:
+            self.notes_on[note] = True
+
+        # Otherwise set the note as off in our records.
+        else:
+            del self.notes_on[note]
+
+        # Send the note message to the OSC server.
+        self.client.send_message("/vkb_midi/" + str(chan) +
+                                 "/note/" + str(note), vel)
+
+    def stop_note(self, note, chan=None):
+        """ Stop a note by setting its velocity to zero. """
+        # Use default MIDI channel if chan argument not specified.
+        if chan == None:
+            chan = self.chan
+
+        # Set the note as off in our records.
+        del self.notes_on[note]
+
+        # Send the note message to the OSC server to turn off the note.
+        self.client.send_message("/vkb_midi/" + str(chan) +
+                                 "/note/" + str(note), 0)
+
+    def stop_all_notes(self, chan=None):
+        """ Send velocity 0 to all notes that are currently playing, to turn them off. 
+            This is being used as a trap function that runs when you press ctrl-c, 
+            so there aren't any lingering notes when the program exits. """
+        # Use default MIDI channel if chan argument not specified.
+        if chan == None:
+            chan = self.chan
+
+        # Stop all the notes that are currently playing.
+        for note in list(self.notes_on):
+            self.stop_note(note, chan)
+
+    def signal_handler(self, sig, frame):
+        """ This runs when you press ctrl-c to stop the program. """
+        print('You pressed ctrl-c. Turning off all notes and quitting...')
+
+        # Stop all the notes that are currently playing.
+        self.stop_all_notes()
+
+        # Exit the program indicating no error.
+        sys.exit(0)
---
+++ examples/play_midi_notes.py
@@ -0,0 +1,81 @@
+# Example by Daphne Volante 2018
+
+# Add one directory level up to the Python module search path.
+# Only needed if your Python file is in a subdirectory.
+if __name__ == '__main__' and __package__ is None:
+    from os import sys, path
+    sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+
+# Import the Moss Bitwig OSC API client library, and some other stuff.
+from bitwig_osc import BitwigOSC
+import argparse
+import time
+
+
+def run(bw):
+    """ This is where we use the BitwigOSC instance to send our OSC messages. """
+    note = 15   # The note to start on.
+    asc = True  # Start ascending.
+    n = 100
+
+    # Loop n times.
+    for _ in range(n):
+        note1 = 0   # The first note to play next.
+        note2 = 0   # The second note to play next.
+
+        # If we reach the top, start descending.
+        if asc and note >= 88:
+            asc = False
+
+        # If we reach the bottom, start ascending.
+        elif not asc and note <= 14:
+            asc = True
+
+        # If we are ascending.
+        if asc:
+            note += 5
+            note1 = note
+            note -= 3
+            note2 = note
+
+        # If we are descending.
+        else:
+            note -= 5
+            note1 = note
+            note += 3
+            note2 = note
+
+        # Play the first note.
+        bw.play_note(note1, 127)
+        time.sleep(0.1)
+        bw.stop_note(note1)
+        time.sleep(0.1)
+
+        # Play the second note.
+        bw.play_note(note2, 127)
+        time.sleep(0.1)
+        bw.stop_note(note2)
+        time.sleep(0.1)
+
+
+# We want to accept some arguments from the command line.
+parser = argparse.ArgumentParser()
+
+# OSC Server IP.
+parser.add_argument("--ip", default="127.0.0.1",
+                    help="The IP of the OSC server.")
+
+# OSC Server port.
+parser.add_argument("--port", type=int, default=8000,
+                    help="The port the OSC server is listening on.")
+
+# Default MIDI channel to use.
+parser.add_argument("--chan", type=int, default=1,
+                    help="The default MIDI channel to send all commands to.")
+args = parser.parse_args()
+
+# Instantiate our Bitwig OSC client library with the command line arguments.
+bw = BitwigOSC(args.ip, args.port, args.chan)
+
+# Run the main routine.
+run(bw)
